e86ba8e34243165cca3942d713c692884591714a
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2020-10-31 13:50:22 +0700

    Add Python krathong in krathongList.js

--- src/krathongList.js
+++ src/krathongList.js
@@ -2,6 +2,10 @@ const krathongList = [
     {
         name: "Placeholder",
         image: require("./assets/logo.png")
+    },
+    {
+        name : "Python",
+        image : require("./assets/Krathong_Python.png")
     }
 ]
 export default krathongList
